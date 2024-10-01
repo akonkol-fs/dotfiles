96a8932124290981ae5041194ae3fc46ca411abf
Andrew Konkol <andrewkonkol@fullstory.com>
2024-10-01 15:38:34 -0500

    add a bunch of kitty themes, etc

--- .config/nvim/lua/akonkol/maps.lua
+++ .config/nvim/lua/akonkol/maps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>nt", function () vim.cmd.Neotree("toggle","current") end)
 vim.keymap.set("n", "<leader>qq", function () vim.cmd("qa") end)
 vim.keymap.set({"n", "v"}, "<leader>cc", "gcc", {remap = true})
+vim.keymap.set({"n", "v"}, "<leader>ls", ":Lazy sync<CR>", {remap = true})
--- .config/nvim/lua/akonkol/settings.lua
+++ .config/nvim/lua/akonkol/settings.lua
@@ -13,6 +13,8 @@ o.scrolloff = 12
 o.undofile = true
 o.undoreload = 10000
 o.cmdheight = 0
+o.termguicolors = true
+
 -- Run GoFmt when saving .go files
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -22,3 +24,44 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
   group = format_sync_grp,
 })
+
+local colorschemes = vim.fn.getcompletion("", "color")
+local colorschemes_idx = vim.fn.index(
+    colorschemes,
+    vim.api.nvim_cmd({ cmd = "colorscheme" }, { output = true })
+)
+
+local function change_colorscheme(forward)
+    if forward then
+        colorschemes_idx = colorschemes_idx + 1
+    else
+        colorschemes_idx = colorschemes_idx - 1
+    end
+
+    if colorschemes_idx > #colorschemes then
+        colorschemes_idx = 1
+    elseif colorschemes_idx < 1 then
+        colorschemes_idx = #colorschemes
+    end
+
+    local ok = pcall(function()
+        vim.cmd("colorscheme " .. colorschemes[colorschemes_idx])
+	vim.print(vim.inspect(colorschemes))
+    end)
+
+    if not ok then
+        change_colorscheme(forward)
+    end
+
+    print(colorschemes[colorschemes_idx])
+end
+
+vim.keymap.set("n", "<C-h>", function()
+    change_colorscheme(true)
+end)
+
+vim.keymap.set("n", "<C-l>", function()
+    change_colorscheme(false)
+end)
+
+vim.cmd("colorscheme kanagawa-dragon")
