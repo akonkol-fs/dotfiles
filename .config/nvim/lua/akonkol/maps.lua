dab4da2ed9c7245ae617f87e9c505eeeaf340367
Andrew Konkol <andrewkonkol@fullstory.com>
2024-10-03 13:05:52 -0500

    replace telescope with fzf

--- .config/nvim/lua/akonkol/maps.lua
+++ .config/nvim/lua/akonkol/maps.lua
@@ -1,4 +1,19 @@
-vim.keymap.set("n", "<leader>nt", function () vim.cmd.Neotree("toggle","current") end)
-vim.keymap.set("n", "<leader>qq", function () vim.cmd("qa") end)
-vim.keymap.set({"n", "v"}, "<leader>cc", "gcc", {remap = true})
-vim.keymap.set({"n", "v"}, "<leader>ls", ":Lazy sync<CR>", {remap = true})
+vim.keymap.set("n", "<leader>nt", function()
+	vim.cmd.Neotree("toggle", "current")
+end)
+vim.keymap.set("n", "<leader>qq", function()
+	vim.cmd("qa")
+end)
+
+-- block comment toggle
+vim.keymap.set({ "n", "v" }, "<leader>cc", "gcc", { remap = true })
+
+-- Install nvim plugins
+vim.keymap.set({ "n", "v" }, "<leader>ls", ":Lazy sync<CR>", { remap = true })
+
+-- fzf files in cwd
+vim.keymap.set("n", "<LocalLeader>ff", ":FzfLua git_files<CR>", { desc = "Fzf Git Files" })
+
+-- Preview colorschemes
+vim.keymap.set({ "n", "v" }, "<leader>cs", ":FzfLua colorschemes<CR>", { remap = true })
+vim.keymap.set("n", "<LocalLeader>be", ":FzfLua buffers<CR>", { desc = "Show active buffers" })
--- .config/nvim/lua/akonkol/settings.lua
+++ .config/nvim/lua/akonkol/settings.lua
@@ -3,7 +3,7 @@ local o = vim.opt
 
 o.number = true
 o.swapfile = true
-o.dir = '/tmp'
+o.dir = "/tmp"
 o.smartcase = true
 o.laststatus = 2
 o.hlsearch = true
@@ -18,50 +18,11 @@ o.termguicolors = true
 -- Run GoFmt when saving .go files
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimports()
-  end,
-  group = format_sync_grp,
+	pattern = "*.go",
+	callback = function()
+		require("go.format").goimports()
+	end,
+	group = format_sync_grp,
 })
-
-local colorschemes = vim.fn.getcompletion("", "color")
-local colorschemes_idx = vim.fn.index(
-    colorschemes,
-    vim.api.nvim_cmd({ cmd = "colorscheme" }, { output = true })
-)
-
-local function change_colorscheme(forward)
-    if forward then
-        colorschemes_idx = colorschemes_idx + 1
-    else
-        colorschemes_idx = colorschemes_idx - 1
-    end
-
-    if colorschemes_idx > #colorschemes then
-        colorschemes_idx = 1
-    elseif colorschemes_idx < 1 then
-        colorschemes_idx = #colorschemes
-    end
-
-    local ok = pcall(function()
-        vim.cmd("colorscheme " .. colorschemes[colorschemes_idx])
-	vim.print(vim.inspect(colorschemes))
-    end)
-
-    if not ok then
-        change_colorscheme(forward)
-    end
-
-    print(colorschemes[colorschemes_idx])
-end
-
-vim.keymap.set("n", "<C-h>", function()
-    change_colorscheme(true)
-end)
-
-vim.keymap.set("n", "<C-l>", function()
-    change_colorscheme(false)
-end)
 
 vim.cmd("colorscheme kanagawa-dragon")
